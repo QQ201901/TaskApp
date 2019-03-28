4012e49140765ff9322c3454c9ea683ff602b120
QQ <oldcapital1990@gmail.com>
2019-03-29 08:04:09 +0900

    テキスト分

---
+++ taskapp/File.swift
@@ -0,0 +1,31 @@
+//
+//  File.swift
+//  taskapp
+//
+//  Created by KUNI K on 2019/03/27.
+//  Copyright © 2019 QQ201901. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+    
+    // タイトル
+    @objc dynamic var title = ""
+    
+    // 内容
+    @objc dynamic var contents = ""
+    
+    /// 日時
+    @objc dynamic var date = Date()
+    
+    /**
+     id をプライマリーキーとして設定
+     */
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
+
